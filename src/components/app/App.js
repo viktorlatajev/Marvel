9b5d5d377965dc7c52b6b8292387ed185597e779
Viktoria <vika.perova.88.88@mail.ru>
2021-10-11 11:21:46 +0300

    dodelatj

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -38,3 +38,5 @@ const App = () => {
         </Router>
     )
 }
+
+export default App;
--- src/components/appHeader/AppHeader.js
+++ src/components/appHeader/AppHeader.js
@@ -1,5 +1,5 @@
 import { Link, NavLink } from "react-router-dom";
-import './apHeader.scss';
+import './appHeader.scss';
 
 const AppHeader = () => {
     return (
